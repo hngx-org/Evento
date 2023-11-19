4e383df58dacb71d6bd7e3fde7043842310063a6
highb33kay <hiibeekayvibe@gmail.com>
2023-11-19 10:53:41 +0100

    Update Swagger definition to use OpenAPI 3.1.0 and add server and license information

--- src/swagger.ts
+++ src/swagger.ts
@@ -1,12 +1,26 @@
 import swaggerJsdoc from "swagger-jsdoc";
 
 const options = {
-  swaggerDefinition: {
-    swagger: "2.0",
+  definition: {
+    openapi: "3.1.0",
     info: {
-      title: "Evento Swagger",
+      title: "Evento API End Point Documentation",
       version: "1.0.0",
       description: "API for Evento API End Point Documentation",
+    },
+    servers: [
+      {
+        url: "http://localhost:3000/",
+        description: "Local server",
+      },
+      {
+        url: "https://your-live-server-url.com/",
+        description: "Live server",
+      },
+    ],
+    license: {
+      name: "Apache 2.0",
+      url: "https://www.apache.org/licenses/LICENSE-2.0.html",
     },
   },
   apis: ["./src/routes/*.route.ts"],
