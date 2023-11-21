058f79f750988cd25f369d44c3e8ea456d3ad83f
abdullmustyy <abdulmusty03@outlook.com>
2023-11-22 00:20:47 +0100

    Feat: Implement get all events route

--- src/controllers/events.controller.ts
+++ src/controllers/events.controller.ts
@@ -89,6 +89,19 @@ const getEventController: RequestHandler = async (req, res, next) => {
   }
 };
 
+// Controller for getting all events
+const getAllEventsController: RequestHandler = async (req, res, next) => {
+  try {
+    // Get all events
+    const allEvents = await event.findMany();
+
+    // Return all events as the response
+    ResponseHandler.success(res, allEvents, 200, "Events found.");
+  } catch (error) {
+    next(error);
+  }
+};
+
 // Controller for editing events
 const editEventController: RequestHandler = async (req, res, next) => {
   try {
@@ -157,4 +170,9 @@ const editEventController: RequestHandler = async (req, res, next) => {
   }
 };
 
-export { createEventController, editEventController, getEventController };
+export {
+  createEventController,
+  getEventController,
+  getAllEventsController,
+  editEventController,
+};
--- src/routes/events.route.ts
+++ src/routes/events.route.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import {
   createEventController,
   getEventController,
+  getAllEventsController,
   editEventController,
 } from "../controllers/events.controller";
 
@@ -12,6 +13,9 @@ eventsRouter.post("/events/create", createEventController);
 
 // Get a single event route
 eventsRouter.get("/events/:eventID", getEventController);
+
+// Get all events route
+eventsRouter.get("/events", getAllEventsController);
 
 // Edit an event route
 eventsRouter.put("/events/edit/:eventID", editEventController);
