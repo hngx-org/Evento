1e6eb31e0630221f785dd466bf8869c59927a33e
highb33kay <hiibeekayvibe@gmail.com>
2023-11-21 16:50:32 +0100

    Add updateUserProfileById function to user.controller.ts

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,8 +1,13 @@
 import express, { Router } from "express";
-import { getUserProfileById } from "./../controllers/user.controller";
+import {
+  getUserProfileById,
+  updateUserProfileById,
+} from "./../controllers/user.controller";
 
 const router: Router = express.Router();
 
 router.get("/user/:id", getUserProfileById);
+
+router.patch("/user/:id", updateUserProfileById);
 
 module.exports = router;
