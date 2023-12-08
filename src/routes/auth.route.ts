ec9ffcabc12bbc112d4b630bc2b9eac9b9cf8517
Erickb <wr.t8r1@gmai.com>
2023-12-07 16:40:39 -0800

    add redirect to dashboard after login

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -163,7 +163,7 @@ router.get(
     res.cookie("token", token, { maxAge: 7 * 24 * 60 * 60 * 1000 });
     
     res.cookie("userId", userID, { maxAge: 7 * 24 * 60 * 60 * 1000 });
-    res.redirect("https://evento1.vercel.app/loading");
+    res.redirect("https://evento1.vercel.app/event-dashboard");
   }
 );
 
